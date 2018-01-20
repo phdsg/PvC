7b5b741def23bc7eb630251fa57c059ea0b1b742
phdsg <phdsgvnchfhn@gmail.com>
2018-01-20 22:35:17 +0100

    SlimSeq: now returns to sequence position when leaving random. new rand mode.

--- src/SlimSeq.cpp
+++ src/SlimSeq.cpp
@@ -5,20 +5,21 @@
 16 step seq (based on m.lueders' sequential switch)
 
 TODO:
-	momentary control inputs (instead of flips)
-	virtual position count when in random mode so we return to the "right"
-	 point in the sequence when leaving random mode
-	unipolar/bipolar options
-	(maybe) gates
-	loopmodes: ping-pong, one-shot
-	different random play mode: coin-toss direction
-	sequence length control
+	-(maybe) momentary control inputs (instead of flips) / optional
+	-(maybe) unipolar/bipolar options
+	-(maybe) gates
+	-loopmodes: ping-pong, one-shot
+	-sequence length control
+	-output slew (portamento)
+	-useful unclocked/held operation
+	-direction/mode lights
+	-mode serialization
 
 */////////////////////////////////////////////////////////////////////////////
 
 #include "pvc.hpp"
 
-#include "dsp/digital.hpp" // SchmittTrigger
+#include "dsp/digital.hpp" // SchmittTrigger PulseGenerator
 
 #define STEPCOUNT 16
 
@@ -33,6 +34,7 @@ struct SlimSeq : Module {
 		RNDMODE_UI,
 		HOLD_UI,
 		RESET_UI,
+
 		NUM_PARAMS
 	};
 
@@ -49,6 +51,7 @@ struct SlimSeq : Module {
 
 	enum OutputIds {
 		OUT,
+
 		NUM_OUTPUTS
 	};
 
@@ -59,6 +62,7 @@ struct SlimSeq : Module {
 		STEP1_LIGHT,
 		OUT_POS_LED = STEP1_LIGHT + STEPCOUNT,
 		OUT_NEG_LED,
+
 		NUM_LIGHTS
 	};
 
@@ -69,24 +73,29 @@ struct SlimSeq : Module {
 	void step() override;
 
 	int currentPos = 0;
+	int counterPos = 0;
 	int resetPos = 0;
 
+	bool isRunning = false;
 	bool isHold = false;
 	bool isRandom  = false;
 	bool isReverse = false;
+	bool isHopper = false;
 
 	SchmittTrigger clockTrigger, reverseTrigger, randomTrigger, holdTrigger, resetTrigger;
-
 	SchmittTrigger clockButton, reverseButton, randomButton, holdButton, resetButton;
-
 	SchmittTrigger posButtons[STEPCOUNT];
 
 	void reset() override {
 		currentPos = 0;
+		counterPos = 0;
 		resetPos = 0;
+
+		isRunning = false;
 		isHold = false;
 		isRandom = false;
 		isReverse = false;
+		isHopper = false;
 		
 		for(int i=0; i<STEPCOUNT; i++){
 			lights[STEP1_LIGHT + i].value = 0.0f;
@@ -98,68 +107,88 @@ struct SlimSeq : Module {
 void SlimSeq::step() {
 	float out = 0.0f;
 
-	// playmodes
-	if(inputs[CLOCK_IN].active && !isHold) {
+	isRunning = (isHold) ? false : inputs[CLOCK_IN].active;
+	// clocked
+	if(isRunning) {
 		if (clockTrigger.process(inputs[CLOCK_IN].value) ) {
+			counterPos += (isReverse) ? -1 : 1;
+
 			if (isRandom) {
-				currentPos = roundf(randomf()*15.0f);
+				if (isHopper) {
+					currentPos += roundf(randomf()*15.0f);
+				}
+				else {
+					currentPos += (roundf(randomf())) ? 1 : -1;
+				}
 			}
 			else {
-				if (isReverse)
-					currentPos--;
-				else
-					currentPos++;
+				currentPos = counterPos;
 			}
 		}
 	}
 
 	// playmode triggers & controls
-	if (reverseTrigger.process(inputs[REVERSE_IN].value)) isReverse  = !isReverse;
-	if (reverseButton.process(params[REVERSE_UI].value)) isReverse  = !isReverse;
-	
+	if (isRandom) {	
+		if (reverseTrigger.process(inputs[REVERSE_IN].value)) isHopper  = !isHopper;
+		if (reverseButton.process(params[REVERSE_UI].value)) isHopper = !isHopper;
+	}
+	else {
+		if (reverseTrigger.process(inputs[REVERSE_IN].value)) isReverse  = !isReverse;
+		if (reverseButton.process(params[REVERSE_UI].value)) isReverse  = !isReverse;
+	}
+
 	if (randomTrigger.process(inputs[RNDMODE_IN].value)) isRandom  = !isRandom;
 	if (randomButton.process(params[RNDMODE_UI].value)) isRandom  = !isRandom;
 
 	if (holdTrigger.process(inputs[HOLD_IN].value)) isHold = !isHold;
 	if (holdButton.process(params[HOLD_UI].value)) isHold = !isHold;
 
-	if (resetTrigger.process(inputs[RESET_IN].value)) currentPos = resetPos;
-	if (resetButton.process(params[RESET_UI].value)) currentPos = resetPos;
+	if (resetTrigger.process(inputs[RESET_IN].value)) {
+		 counterPos = resetPos;
+		 currentPos = resetPos;
+	}
+	if (resetButton.process(params[RESET_UI].value)) {
+		counterPos = resetPos;
+		currentPos = resetPos;
+	}
 
 	// position selectors
 	for(int i=0; i<STEPCOUNT; i++) {
 		if(posButtons[i].process(params[STEP1_SEL+i].value)) {
 			resetPos = i;
-			currentPos = (!inputs[CLOCK_IN].active || isHold) ? i : currentPos;
+			currentPos = (!isRunning) ? i : currentPos;
 		}
 	}
 
-	// do the steps
+	// loop the counter
+	while ( counterPos < 0 )
+		counterPos += STEPCOUNT;
+	while ( counterPos >= STEPCOUNT )
+		counterPos -= STEPCOUNT;
+
+	// loop the current
 	while ( currentPos < 0 )
 		currentPos += STEPCOUNT;
 	while ( currentPos >= STEPCOUNT )
 		currentPos -= STEPCOUNT;
+
 	
 	// calc out
-	out = (inputs[STEP1_IN+currentPos].active) ? // ...
-		inputs[STEP1_IN+currentPos].normalize(0.0f) * (params[STEP1_KNOB+currentPos].value * 0.2f)
-		: params[STEP1_KNOB+currentPos].value;
-
+	out = inputs[STEP1_IN+currentPos].normalize(5.0f) * params[STEP1_KNOB+currentPos].value;
 	outputs[OUT].value = clampf(out * params[OUT_KNOB].value, -10.0f, 10.0f);
 
 	// lights
 		// direction
-	lights[REVERSE_LIGHT].setBrightnessSmooth(isHold ? 1.0f : isRandom ? 0.0f : isReverse ? 1.0f : 0.0f);
-	lights[RNDMODE_LIGHT].setBrightnessSmooth(isHold ? 1.0f : isRandom ? 1.0f : 0.0f);
-	lights[FORWARD_LIGHT].setBrightnessSmooth(isHold ? 1.0f : isRandom ? 0.0f : isReverse ? 0.0f : 1.0f);
-		// steps
+	lights[REVERSE_LIGHT].value = isHold ? 1.0f : isRandom ? 0.0f : isReverse ? 1.0f : 0.0f;
+	lights[RNDMODE_LIGHT].value = isHold ? 1.0f : isRandom ? 1.0f : 0.0f;
+	lights[FORWARD_LIGHT].value = isHold ? 1.0f : isRandom ? 0.0f : isReverse ? 0.0f : 1.0f;
+		// steplights
 	for(int i=0; i<STEPCOUNT; i++) {
-		lights[STEP1_LIGHT+i].setBrightnessSmooth(i == currentPos ? 1.0f:0.0f);
-		lights[STEP1_LIGHT+i].setBrightnessSmooth(i == resetPos ? 0.5f:0.0f);
+		lights[STEP1_LIGHT+i].value = (i == currentPos) ? 1.0f : (i == resetPos) ? 0.5f:0.0f;
 	}
 		// out
-	lights[OUT_POS_LED].setBrightnessSmooth(outputs[OUT].value < 0 ? -outputs[OUT].value*0.2f : 0.0f);
-	lights[OUT_NEG_LED].setBrightnessSmooth(outputs[OUT].value > 0 ?  outputs[OUT].value*0.2f : 0.0f);
+	lights[OUT_POS_LED].value = (outputs[OUT].value < 0) ? -outputs[OUT].value*0.2f : 0.0f;
+	lights[OUT_NEG_LED].value = (outputs[OUT].value > 0) ?  outputs[OUT].value*0.2f : 0.0f;
 };
 
 template <typename BASE>
@@ -168,10 +197,9 @@ struct PvCBigLight : BASE {
 		this->box.size = Vec(22, 22);
 	}
 };
-struct BlueOrangeLight : ModuleLightWidget {
-	BlueOrangeLight() {
-		addBaseColor(COLOR_BLUE);
-		addBaseColor(COLOR_ORANGE);
+struct PvCKnobT : PvCKnob {
+	PvCKnobT() {
+		setSVG(SVG::load(assetPlugin(plugin, "res/components/PvCKnobT.svg")));
 	}
 };
 struct StepButton : SVGSwitch, MomentarySwitch {
@@ -183,11 +211,6 @@ struct StepButton : SVGSwitch, MomentarySwitch {
 struct ControlButton : StepButton {
 	ControlButton() {
 		box.size = Vec(24,14);
-	}
-};
-struct PvCKnobT : PvCKnob {
-	PvCKnobT() {
-		setSVG(SVG::load(assetPlugin(plugin, "res/components/PvCKnobT.svg")));
 	}
 };
 
@@ -211,16 +234,16 @@ SlimSeqWidget::SlimSeqWidget() {
 	// steps
 	for (int i = 0; i < 8; i++)	{
 		int top = 42;
-		// left
-		addChild(createLight<PvCBigLight<BlueLight>>(Vec(4,22 + top * i),module,SlimSeq::STEP1_LIGHT + STEPCOUNT-1 - i));
-		addParam(createParam<PvCKnobT>(Vec(4,22 + top * i),module,SlimSeq::STEP1_KNOB + STEPCOUNT-1 - i , -5.0f, 5.0f, 0.0f));
-		addInput(createInput<InPortAud>(Vec(18,42 + top * i),module,SlimSeq::STEP1_IN + + STEPCOUNT-1 - i));
+		// left 16-9
+		addChild(createLight<PvCBigLight<BlueLight>>(Vec(18,42 + top * i),module,SlimSeq::STEP1_LIGHT + STEPCOUNT-1 - i));
+		addParam(createParam<PvCKnobT>(Vec(18,42 + top * i),module,SlimSeq::STEP1_KNOB + STEPCOUNT-1 - i , -1.0f, 1.0f, 0.0f));
+		addInput(createInput<InPortAud>(Vec(4,22 + top * i),module,SlimSeq::STEP1_IN + + STEPCOUNT-1 - i));
 		addParam(createParam<StepButton>(Vec(29,29 + top * i),module,SlimSeq::STEP1_SEL + STEPCOUNT-1 - i , 0, 1, 0));
-		// right
+		// right 1-8
 		addParam(createParam<StepButton>(Vec(80,29 + top * i),module,SlimSeq::STEP1_SEL + i , 0, 1, 0));
-		addChild(createLight<PvCBigLight<BlueLight>>(Vec(94,22 + top * i),module,SlimSeq::STEP1_LIGHT + i));
-		addInput(createInput<InPortAud>(Vec(80,42 + top * i),module,SlimSeq::STEP1_IN + i));
-		addParam(createParam<PvCKnobT>(Vec(94,22 + top * i),module,SlimSeq::STEP1_KNOB + i, -5.0f, 5.0f, 0.0f));
+		addChild(createLight<PvCBigLight<BlueLight>>(Vec(80,42 + top * i),module,SlimSeq::STEP1_LIGHT + i));
+		addInput(createInput<InPortAud>(Vec(94,22 + top * i),module,SlimSeq::STEP1_IN + i));
+		addParam(createParam<PvCKnobT>(Vec(80,42 + top * i),module,SlimSeq::STEP1_KNOB + i, -1.0f, 1.0f, 0.0f));
 	}
 	// direction lights
 	addChild(createLight<SmallLight<OrangeLight>>(Vec(49,22),module,SlimSeq::REVERSE_LIGHT));
@@ -238,7 +261,7 @@ SlimSeqWidget::SlimSeqWidget() {
 	addInput(createInput<InPortBin>(Vec(49,216),module,SlimSeq::RESET_IN));
 	addParam(createParam<ControlButton>(Vec(48,238),module,SlimSeq::RESET_UI , 0, 1, 0));
 	// main out
-	addChild(createLight<PvCBigLight<BlueOrangeLight>>(Vec(49,286),module,SlimSeq::OUT_POS_LED));
-	addParam(createParam<PvCKnobT>(Vec(49,286),module,SlimSeq::OUT_KNOB, -1.0f, 1.0f, 1.0f));
+	addChild(createLight<PvCBigLight<GreenRedLight>>(Vec(49,292),module,SlimSeq::OUT_POS_LED));
+	addParam(createParam<PvCKnobT>(Vec(49,292),module,SlimSeq::OUT_KNOB, -1.0f, 1.0f, 1.0f));
 	addOutput(createOutput<OutPortVal>(Vec(49,326),module,SlimSeq::OUT));
 }
